8f6fd851c6f0dc1a01290408e3b34d2a11fef0ca
Amanda Gray <floridabelle2021@protonmail.com>
2023-03-28 17:45:08 -0600

    made an outline of what the criteria of my code that is needed

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,18 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("I clicked the button")// Tested clicking the button
+
+//Need to prompt user for the password criteria
+// -password length
+// -criteria characters needed
+// -make sure the users input is valid
+// -generate password
+
+
+
+
+// -display password to the page
+return "Generated Password"//Tested the "Return" generated password
 }
 
 function writePassword() {
