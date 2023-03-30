72c635d0eb501a34f6c255fe34ee6fa097b63c7f
Amanda Gray <floridabelle2021@protonmail.com>
2023-03-29 23:06:44 -0600

    adjustments to return statements

--- Develop/script.js
+++ Develop/script.js
@@ -10,95 +10,93 @@ var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var generateBtn = document.querySelector("#generate");
 
 //I added "Event listener" to generate the button
-generateBtn.addEventListener("click",typePassword);
+generateBtn.addEventListener("click", typePassword);
 
 //prompts will be either true or false
 function typePassword() {
-    var correctPrompts = getPrompts();
-    var passwordText = document.querySelector("#password");
+  var correctPrompts = getPrompts();
+  var passwordText = document.querySelector("#password");
 
   if (correctPrompts) {
-     var newPassword = generatePassword();
-     passwordText.value = newPassword;
-    }
-else {
-  passwordTest.value = "";
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordTest.value = "";
+  }
 }
+
+function generatePassword() {
+  console.log("I clicked the button") // Tested clicking the button
+  var password = "";
+
+  for (var x = 0; x < characterLength; x++) {
+    var randomIndex = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomIndex]
   }
+  console.log("password", password)
+  return password;
+}
 
-  function generatePassword() {
-    // console.log("I clicked the button") // Tested clicking the button
-    var password = "";
+// Added a "parseInt"to convert the values into integers
+// Testing to see if this is ALL false
+function getPrompts() {
+  choiceArray = [];
 
-    for (var x = 0; x < characterLength; x++) {
-      var randomIndex = Math.floor(Math.random()* choiceArray.length);
-      password - password + choiceArray[randomIndex]
-    }
-    return password;
+  // NaN below
+  characterLength = parseInt(prompt("How many characters would you like in your paasssword? (8 - 128 characters"));
+
+
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Your character length needs to be a Number, 8 - 128 digets. Please try again.");
+    return false;
   }
+  // User has four promt questions to help create a new password 
+  if (confirm("Do you want to use special characters in your password?")) {
+    choiceArray = choiceArray.concat(specialCharacterArray);
+    console.log("choiceArray", choiceArray)
+  }
+  if (confirm("Do you want to use lowercase letters in your password?")) {
+    choiceArray = choiceArray.concat(lowerCaseArray);
+    console.log("choiceArray", choiceArray)
 
-    // Added a "parseInt"to convert the values into integers
-    // Testing to see if this is ALL false
-    function getPrompts() {
-      choiceArray = [];
+  }
+  if (confirm("Do you want to use upper case letters in your password?")) {
+    choiceArray = choiceArray.concat(upperCaseArray);
+    console.log("choiceArray", choiceArray)
 
-      // NaN below
-      characterLength = parseInt(prompt("How many characters would you like in your paasssword? (8 - 128 characters"));
-     
+  }
+  if (confirm("Do you want to use numbers in your password?")) {
+    choiceArray = choiceArray.concat(numberArray);
+    console.log("choiceArray", choiceArray)
+  }
+ 
+  // retur n true;
+  return choiceArray //Tested the "Return" generated password
+}
 
-      if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-        alert("Your character length needs to be a Number, 8 - 128 digets. Please try again.");
-        return false;
-      }
-      // User has four promt questions to help create a new password 
-      if (confirm("Do you want to use special characters in your password?")) {
-        choiceArray = choiceArray.concat(specialCharacterArray);
 
-      }
-      if (confirm("Do you want to use lowercase letters in your password?")) {
-        choiceArray = choiceArray.concat(lowerCaseArray);
-
-      }
-      if (confirm("Do you want to use upper case letters in your password?")) {
-        choiceArray = choiceArray.concat(upperCaseArray);
-
-      }
-      if (confirm("Do you want to use numbers in your password?")) {
-        choiceArray = choiceArray.concat(numberArray);
-
-      }
-      return true;
-      return "Generated Password"//Tested the "Return" generated password
-
-      // Need to prompt user for the password criter
-      var password = prompt("Your Secure Password");
-
-    }
-
-  
 
 
 //PROMTUSER????
 
 
-    
-    
 
 
-    // // -password length
-    // // -criteria characters needed
-    // // -make sure the users input is valid
-    // // -generate password
-    // // -display password to the page
 
 
-    // resourcces
+// // -password length
+// // -criteria characters needed
+// // -make sure the users input is valid
+// // -generate password
+// // -display password to the page
 
-    // https://stackoverflow.com/questions/17907455/how-to-get-numeric-value-from-a-prompt-box
 
-    //https://www.w3docs.com/learn-javascript/javascript-simple-actions.html
+// resourcces
 
-    //https://www.geeksforgeeks.org/nan-not-number-java/
+// https://stackoverflow.com/questions/17907455/how-to-get-numeric-value-from-a-prompt-box
 
-    //https://www.w3schools.com/jsref/jsref_obj_array.asp
+//https://www.w3docs.com/learn-javascript/javascript-simple-actions.html
 
+//https://www.geeksforgeeks.org/nan-not-number-java/
+
+//https://www.w3schools.com/jsref/jsref_obj_array.asp
